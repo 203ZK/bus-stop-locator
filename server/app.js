a6009f9df29d422b19a23ee5f604ae4359912cef
Hou Jin <houjinwork@gmail.com>
2024-12-08 18:51:19 +0800

    Linked frontend to server.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+const data = {
+    stops: [
+        { name: "Opp Tanglin Halt", code: 43013 },
+        { name: "Blk 709", code: 39483 },
+        { name: "Newton Stn Exit A", code: 92432 }
+    ]
+};
+
+app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.json(data);
 });
 
 const PORT = process.env.PORT || 3000;
